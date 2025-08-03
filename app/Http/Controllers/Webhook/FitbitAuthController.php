6e325e43a013111390b4e73a06f5bfd1de2a8e7d
Manoj Bisht <manoj.webartisan@gmail.com>
2025-08-03 21:53:18 +0530

    Fix FitbitAuthController path

--- app/Http/Controllers/Webhook/FitbitAuthController.php
+++ app/Http/Controllers/Webhook/FitbitAuthController.php
@@ -1,43 +1,49 @@
 <?php
+
+declare(strict_types=1);
 /**
  * @Deprecated
  * I will be removed once changes are migrated
  */
-namespace App\Http\Controller\Webhook;
+
+namespace App\Http\Controllers\Webhook;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-class FitbitAuthController extends Controller
+final class FitbitAuthController extends Controller
 {
     // Fitbit API credentials
     private $clientId;
+
     private $clientSecret;
+
     private $redirectUri;
 
-    public function __construct(){
+    public function __construct()
+    {
         // Fitbit API credentials
         $this->clientId = env('FITBIT_CLIENT_ID');
         $this->clientSecret = env('FITBIT_CLIENT_SECRET');
-        $this->redirectUri = env('FITBIT_REDIRECT_URI',route('fitbit.callback'));
+        $this->redirectUri = env('FITBIT_REDIRECT_URI', route('fitbit.callback'));
     }
 
     /**
      * Redirect the user to Fitbit's OAuth 2.0 authorization page.
      */
-    public function redirectToFitbit(Request $request,$state = 'web')
+    public function redirectToFitbit(Request $request, $state = 'web')
     {
         session(['loggedin_user_id' => $request->get('uid')]);
 
-        $authorizationUrl = "https://www.fitbit.com/oauth2/authorize";
+        $authorizationUrl = 'https://www.fitbit.com/oauth2/authorize';
         $queryParams = http_build_query([
             'response_type' => 'code',
             'client_id' => $this->clientId,
             'redirect_uri' => $this->redirectUri,
             'scope' => 'activity sleep nutrition settings profile weight', // Adjust scopes as needed
             'expires_in' => '86400',
-            'state' => $state
+            'state' => $state,
         ]);
 
         return redirect("{$authorizationUrl}?{$queryParams}");
@@ -48,7 +54,7 @@ class FitbitAuthController extends Controller
      */
     public function handleCallback(Request $request)
     {
-        $isApp = $request->get('state') == 'app';
+        $isApp = $request->get('state') === 'app';
 
         if ($request->has('error')) {
             return response()->json(['error' => $request->get('error')]);
@@ -56,14 +62,14 @@ class FitbitAuthController extends Controller
 
         $authorizationCode = $request->get('code');
 
-        if (!$authorizationCode) {
+        if (! $authorizationCode) {
             return response()->json(['error' => 'Authorization code missing.']);
         }
 
-        $tokenUrl = "https://api.fitbit.com/oauth2/token";
+        $tokenUrl = 'https://api.fitbit.com/oauth2/token';
 
         $response = Http::asForm()->withHeaders([
-            'Authorization' => 'Basic ' . base64_encode("{$this->clientId}:{$this->clientSecret}"),
+            'Authorization' => 'Basic '.base64_encode("{$this->clientId}:{$this->clientSecret}"),
         ])->post($tokenUrl, [
             'client_id' => $this->clientId,
             'grant_type' => 'authorization_code',
@@ -79,20 +85,20 @@ class FitbitAuthController extends Controller
 
         $this->subscribe($tokens['access_token'], session('loggedin_user_id'), $tokens['user_id']);
 
-        if($isApp) {
-            if($response == false) {
-                return response()->json(['message' => 'Unabled to complete your request'],403);
+        if ($isApp) {
+            if ($response === false) {
+                return response()->json(['message' => 'Unabled to complete your request'], 403);
             }
 
-            //"rte://settings/fitbit/${$tokens['access_token']}/${$tokens['refresh_token']}/${$tokens['expires_in']}/${$tokens['user_id']}"
-            //rte://settings/fitbit/${access_token}/${refresh_token}/${token_expires_at}/${access_token_secret}
+            // "rte://settings/fitbit/${$tokens['access_token']}/${$tokens['refresh_token']}/${$tokens['expires_in']}/${$tokens['user_id']}"
+            // rte://settings/fitbit/${access_token}/${refresh_token}/${token_expires_at}/${access_token_secret}
 
-            return redirect(sprintf("rte://settings/%s/%s/%s/%s/%s/1", $tokens['access_token'],$tokens['refresh_token'],$tokens['expires_in'],$tokens['user_id'],'fitbit'));
+            return redirect(sprintf('rte://settings/%s/%s/%s/%s/%s/1', $tokens['access_token'], $tokens['refresh_token'], $tokens['expires_in'], $tokens['user_id'], 'fitbit'));
 
             return redirect("rte://settings/${$tokens['access_token']}/${$tokens['refresh_token']}/${$tokens['expires_in']}/${$tokens['user_id']}");
         }
 
-//dd($response,$tokens);
+        // dd($response,$tokens);
         // Save tokens in your database or session
         // Example: Save in session for temporary use
         session([
@@ -110,14 +116,14 @@ class FitbitAuthController extends Controller
     {
         $refreshToken = session('fitbit_refresh_token');
 
-        if (!$refreshToken) {
+        if (! $refreshToken) {
             return response()->json(['error' => 'Refresh token missing.']);
         }
 
-        $tokenUrl = "https://api.fitbit.com/oauth2/token";
+        $tokenUrl = 'https://api.fitbit.com/oauth2/token';
 
         $response = Http::asForm()->withHeaders([
-            'Authorization' => 'Basic ' . base64_encode("{$this->clientId}:{$this->clientSecret}"),
+            'Authorization' => 'Basic '.base64_encode("{$this->clientId}:{$this->clientSecret}"),
         ])->post($tokenUrl, [
             'grant_type' => 'refresh_token',
             'refresh_token' => $refreshToken,
@@ -142,7 +148,7 @@ class FitbitAuthController extends Controller
     {
         $subscriptionId = "{$userId}-{$subscriptionId}";
 
-        $response = Http::baseUrl("https://api.fitbit.com/1/")
+        $response = Http::baseUrl('https://api.fitbit.com/1/')
             ->withToken($accessToken)
             ->post("user/-/apiSubscriptions/{$subscriptionId}.json");
 
